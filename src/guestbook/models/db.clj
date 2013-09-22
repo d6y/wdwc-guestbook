9c60fd7a09fa3a1d4c0adcdb109aca06daa98e79
Richard Dallaway <richard@dallaway.com>
2013-09-22 10:10:14 +0100

    Wip

---
+++ src/guestbook/models/db.clj
@@ -0,0 +1,26 @@
+(ns guestbook.models.db
+  (:require [clojure.java.jdbc :as sql])
+  (:import java.sql.DriverManager))
+
+(def db {:classname "org.sqlite.JDBC"
+         :subprotocol "sqlite"
+         :subname "db.sq3"})
+
+(defn create-guestbook-table []
+  (sql/with-connection
+     db
+     (sql/create-table
+      :guestbook
+      [:id "INTEGER PRIMARY KEY AUTOINCREMENT"]
+      [:timestamp "TIMESTAMP DEFAULT CURRENT_TIMESTAMP"]
+      [:name "TEXT"])
+     (sql/do-commands "CREATE INDEX timestamp_index ON guestbook (timestamp)"))
+  )
+
+(defn read-guests []
+  (sql/with-connection
+   db
+   (sql/with-query-results
+    res
+    ["SELECT * FROM guestbook ORDER BY timesteamp DESC"]
+    (doall res))))
